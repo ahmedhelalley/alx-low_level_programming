d1897c6a82bb731396e31ae414b9b6712b55acad
ahmedhelalley <a.helalley@gmail.com>
2023-10-05 17:28:44 +0300

    commit msg

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<unistd.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Here is some description for the program
+ *
+ * Return: 1 on error
+*/
+
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59)
+return (1);
+}
+
