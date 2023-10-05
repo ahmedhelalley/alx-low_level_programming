1f978685371cddc9d2b36ed88618b8d2d55fd3f4
ahmedhelalley <a.helalley@gmail.com>
2023-10-05 16:49:56 +0300

    commit msg

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Here is some description for the program
+ *
+ * Return 0 on success
+*/
+
+int main(){
+
+    printf("with proper grammar, but the outcome is a piece of art,");
+    return (0);
+}
+
