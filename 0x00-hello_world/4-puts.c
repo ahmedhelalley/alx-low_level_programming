b7a3f4d05d51ad84d33b10494477c44523f7a336
ahmedhelalley <a.helalley@gmail.com>
2023-10-05 16:21:01 +0300

    commit msg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Description: A C program that prints using puts function
  *
- * Return: 0 on success*/
+ * Return: 0 on success
+*/
 
 int main(void)
 {
