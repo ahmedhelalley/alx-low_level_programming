4013b6391cacb7f5af7102504e8a93f78308e703
ahmedhelalley <a.helalley@gmail.com>
2023-10-09 00:23:33 +0300

    commit msg

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Here is some description for the program
+ *
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		putchar(a + 48);
+	}
+	putchar('\n');
+return (0);
+}
