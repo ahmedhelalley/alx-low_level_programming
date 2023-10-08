667bdb554ae7bc626546efe4a76e81af5ab9a094
ahmedhelalley <a.helalley@gmail.com>
2023-10-09 00:53:00 +0300

    commit msg

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Here is some description for the program
+ *
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+char digit = 0;
+while (digit <= 9)
+{
+	putchar(digit + 48);
+	if (digit != 9)
+	{
+		putchar(' ');
+		putchar(',');
+	}
+}
+putchar('\n');
+return (0);
+}
