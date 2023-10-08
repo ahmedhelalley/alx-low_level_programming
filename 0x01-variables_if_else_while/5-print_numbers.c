46a0ace397693c119c81042c803f677bc535c3f3
ahmedhelalley <a.helalley@gmail.com>
2023-10-08 22:50:22 +0300

    commit msg

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Here is some description for the program
+ *
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
