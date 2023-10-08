1b4e75fca4bf8d20f0301c29b9cf69e79ae8b4ae
ahmedhelalley <a.helalley@gmail.com>
2023-10-09 00:29:41 +0300

    commit msg

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Here is some description for the program
+ *
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+char ch = 'z'
+
+while (ch >= 97)
+{
+	putchar(ch);
+	ch--;
+}
+putchar('\n');
+return (0);
+}
