d7b41926646921d3661eca89b24487470d4e682c
ahmedhelalley <a.helalley@gmail.com>
2023-10-11 00:42:28 +0300

    commit msg

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,10 @@ int print_last_digit(int x)
 {
 	int lastDigit;
 
-	if (n < 0)
-		lastDigit = -1 * (n % 10);
+	if (x < 0)
+		lastDigit = -1 * (x % 10);
 	else
-		lastDigit = n % 10;
+		lastDigit = x % 10;
 	_putchar(lastDigit + 48);
 	return (lastDigit);
 }
