2ebb2e87d092a90f5b4d8c102f51e90f284b68e6
ahmedhelalley <a.helalley@gmail.com>
2023-10-11 01:42:10 +0300

    commit msg

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - jskndzvlkn kls kndfzlkn dnzl knlkdz
+ *
+ * @a: kldnzfvlknkl kld nflknzkl lk zlkflk
+ * @b: noinweoafi  sk k skz kk zklklolwa o o a
+ *
+ * Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
