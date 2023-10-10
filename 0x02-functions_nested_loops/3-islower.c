d508510f73af4afc66e3965a0740e3da1ef7c4a7
ahmedhelalley <a.helalley@gmail.com>
2023-10-10 22:48:23 +0300

    commit msg

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - function to check lkndv dfj
+ * knklnvzkldnfdv kndlfnvk
+ *
+ * Description - nfzdv fnvjdnv dfznvkjf fj
+ *
+ * @c: checks input
+ *
+ * Return: 1 if and otherwise 0
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
