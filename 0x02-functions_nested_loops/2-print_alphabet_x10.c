420a133084ce51bf5c30841c19e6db3f75c386bb
ahmedhelalley <a.helalley@gmail.com>
2023-10-10 22:29:38 +0300

    commit msg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,11 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char ch;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (char ch = 'a'; ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
