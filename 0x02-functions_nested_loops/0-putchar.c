f0f47d57cbb9c7850be98bc43145aedb971baef6
ahmedhelalley <a.helalley@gmail.com>
2023-10-10 21:19:48 +0300

    commit msg

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Here is some description for the program
+ *
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
