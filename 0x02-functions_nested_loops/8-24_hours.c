2078013893e214e60e4195e9286e575486bfc849
ahmedhelalley <a.helalley@gmail.com>
2023-10-11 00:59:36 +0300

    commit msg

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *jack_bauer - kjsfdnv jkndsv kls
+*/
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; hr++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(58);
+			_putchar((min / 10) + 48);
+			_putchar((min) + 48);
+			_putchar('\n');
+		}
+	}
+}
