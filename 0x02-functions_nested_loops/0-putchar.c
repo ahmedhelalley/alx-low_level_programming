76e604c68e05b34305d7abc119a576a9c3ba9b0d
ahmedhelalley <a.helalley@gmail.com>
2023-10-10 21:46:12 +0300

    commit msg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
  * main - Entry Point
  *
@@ -10,6 +9,12 @@
 
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <std.io>
+int _putchar(char c);
+#endif
