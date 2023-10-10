b6a34bb0833dbeb102c6aa9331e2b099697323ec
ahmedhelalley <a.helalley@gmail.com>
2023-10-10 21:56:45 +0300

    commit msg

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Description: Here is some description for the program
+ *
+ * Return: 0 on success
+*/
+
+int main(void)
+{
+int ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+_putchat('\n');
+return (0);
+}
