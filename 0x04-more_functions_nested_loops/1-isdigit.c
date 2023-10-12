7b5d4cfe3eaeaddd8e1e177e165c0c70f005ae89
ahmedhelalley <a.helalley@gmail.com>
2023-10-12 13:40:11 +0300

    commit msg

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - jksngkjn knfkn kafk kenf knsfekln
+ *
+ * @c: kfdgknesn skdfvk kdfklvklnf sdkf
+ *
+ * Return: 1 kdnfkn skdfkdsnfs 0 ksdn dfsdf df kmd
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
