352a90d317ebe37a49700d75021da56396bf53c1
ahmedhelalley <a.helalley@gmail.com>
2023-10-12 14:28:02 +0300

    commit msg

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - kjngjnvsjd sdnfv nsd
+ * @n: lksdnf sdknf dknf
+*/
+void print_diagonal(int n)
+{
+	int line, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (line = 1; line <= n; line++)
+		{
+			for (space = 1; space <= line; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
