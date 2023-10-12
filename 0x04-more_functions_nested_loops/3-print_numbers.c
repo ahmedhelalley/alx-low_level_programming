a49219a8ad89fb68174721bf6ea4ecd3fc9d6c34
ahmedhelalley <a.helalley@gmail.com>
2023-10-12 13:49:30 +0300

    commit msg

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_numbers - skdnfnsd sdfnk nskdfn
+ * Return: nothing in the things
+*/
+void print_numbers(void)
+{
+	for (char i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
