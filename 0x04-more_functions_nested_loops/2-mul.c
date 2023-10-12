3edfdacfce7f5984488d87208a77d2c43062d1b8
ahmedhelalley <a.helalley@gmail.com>
2023-10-12 13:44:12 +0300

    commit msg

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - sdngvjns skvn dskfd
+ * @a: kfng nkfnk f
+ * @b: sdkfnk alkdfn kadf ksdan
+ * Return: prod df df dsf
+*/
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
