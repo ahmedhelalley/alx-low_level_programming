e014c02326dc2afb443a077425cee81882d7be10
ahmedhelalley <a.helalley@gmail.com>
2023-10-12 13:58:09 +0300

    commit msg

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - irefin iadfsinwadio ioadfidf
+*/
+void print_most_numbers(void)
+{
+	char num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		if ((num != 50) && (num != 52))
+			_putchar(num);
+	}
+	_putchar('\n');
+}
