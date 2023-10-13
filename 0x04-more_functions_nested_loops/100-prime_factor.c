27ee09774121b50b45729f35656704283a22f78f
ahmedhelalley <a.helalley@gmail.com>
2023-10-13 14:59:00 +0300

    commit msg

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <inttypes.h>
+/**
+ * main - ongoinv jdfknnfd djfnvnd
+ * Return: 0 on success
+*/
 
-int main() {
-    uint64_t n = 612852475143;
-    uint64_t largest_prime = 2;
+int main(void)
+{
+uint64_t n = 612852475143;
+uint64_t largest_prime = 2;
 
-    while (n > largest_prime) {
-        if (n % largest_prime == 0) {
-            n /= largest_prime;
-        } else {
-            largest_prime++;
-        }
-    }
-
-    printf("%" PRIu64 "\n", largest_prime);
-
-    return 0;
+while (n > largest_prime)
+{
+if (n % largest_prime == 0)
+{
+n /= largest_prime;
+}
+else
+{
+largest_prime++;
+}
+}
+printf("%" PRIu64 "\n", largest_prime);
+return (0);
 }
 
