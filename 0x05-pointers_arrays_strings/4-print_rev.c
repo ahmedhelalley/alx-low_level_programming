5779126d5804e2d7da5cf1768f1299a44631151b
ahmedhelalley <a.helalley@gmail.com>
2023-10-16 23:58:08 +0300

    commit msg

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - kldnfklndlks kdlflk ndlkfnlkn
+ * @s: dljfklnd dkfnkl sdknfkl
+*/
+void print_rev(char *s)
+{
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(*s[i]);
+	}
+	_putchar('\n');
+}
