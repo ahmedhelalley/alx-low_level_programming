2d4be647f9702600ed8b4c158b4f7a58538f103f
ahmedhelalley <a.helalley@gmail.com>
2023-10-16 23:27:47 +0300

    commit msg

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - lknsjknvsv skdnfo ssdfkno
+ * @a:fsnvo dsjfoi jdsfjo
+ * @b: ldsfnk dfjoijf dsfjoi
+*/
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = *tmp;
+}
