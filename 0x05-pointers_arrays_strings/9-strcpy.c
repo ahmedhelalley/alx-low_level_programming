70efc1008a283d8381fd3247db7faec041d9eb53
ahmedhelalley <a.helalley@gmail.com>
2023-10-17 02:30:38 +0300

    commit msg

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strcpy - ekndvlk kldnfvlknelkd ldaknfl
+ * @dest: kdnf kdfnv dlkfnv
+ * @src: lasjdnf dnfv dofvkevkejfoie
+ * Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	do {
+		dest[i] = scr[i];
+		i++;
+	} while (src[i] != '\0');
+	return (dest);
+}
