a87ac10eeccad075758c7da208aeb3d05b57a48b
ahmedhelalley <a.helalley@gmail.com>
2023-10-17 01:04:01 +0300

    commit msg

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	if (length % 2 == 0)
 		start_index = length / 2;
 	else
-		start_index = (length - 1) / 2;
+		start_index = (length) / 2;
 	for (i = start_index; str[i] != '\0'; i++)
 	{
 		putchar(str[i]);
