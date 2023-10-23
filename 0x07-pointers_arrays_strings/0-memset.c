71cf9325035651520bd010983ff2228906c5532c
ahmedhelalley <a.helalley@gmail.com>
2023-10-23 21:11:04 +0300

    commit msg

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memset - knckln vskldn lkndlknl
+ * @s: lksdflk lsk lknsv nlkncklx
+ * @n: dsk nklfndkl snkln lkn
+ * @b: lsdkhf lk nlknkl nslkdn
+ * Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
