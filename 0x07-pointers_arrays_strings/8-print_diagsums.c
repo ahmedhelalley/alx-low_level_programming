3db083fe3a39fcdb441a779d986e40e877192b0c
ahmedhelalley <a.helalley@gmail.com>
2023-10-23 22:59:11 +0300

    commit msg

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagsums - shdn hsda oihc oihs doihfhcz
+ * @a: array of jsf hznc jcnj
+ * @size: size of array
+*/
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		sum2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
+}
