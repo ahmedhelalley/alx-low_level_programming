cf164667029a07a8f840151a58cad5acc39a0ea1
ahmedhelalley <a.helalley@gmail.com>
2023-10-25 22:52:22 +0300

    commit msg

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+int _sqrt(int n, int val);
+/**
+ * _sqrt_recursion - skfdj sfkj oesjfdijv oijdsf
+ * @n: erijdf ejsfdg oiejfsdcv
+ * Return: lkjdf sjv sijfcv
+*/
+int _sqrt_recursion(int n)
+{
+	_sqrt(n, 1);
+}
+/**
+ * _sqrt - lkdsf js slkjv iosjfcv
+ * @n: fdsj pjsf jsefvj podjfsvc
+ * @val: lkjdfs oj jdsfv
+ * Return: lkjfds js jdfsvcij
+*/
+int _sqrt(int n, int val)
+{
+	if (val * val == n)
+		return (i);
+	else if (val * val < n)
+		return (_sqrt(n, val + 1));
+	else
+		return (-1);
+}
