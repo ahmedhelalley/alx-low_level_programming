af113e7480c358ff63d64f293f934c0bf2800058
ahmedhelalley <a.helalley@gmail.com>
2023-10-25 22:14:22 +0300

    commit msg

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - skdn jv nsv skjv skjvc
+ * @s: array of strings
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recusrsion(s + 1);
+		putchar(*s);
+	}
+}
