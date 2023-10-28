1523bac3d67f610138e4bcaab2b3b4a1ee93d49b
ahmedhelalley <a.helalley@gmail.com>
2023-10-28 03:04:15 +0200

    commit msg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - owdihsf oijdsa oisjda oishdiof
+ * @argc: kjfd iojfdois oiejfdsoij efs
+ * @argv: eiofdj ijef iejfdvi jefoidfvoicjeds
+ * Return: 0 on success
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
