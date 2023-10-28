e63b3ee2963b2ebccdd1685e07c82502a53ba43e
ahmedhelalley <a.helalley@gmail.com>
2023-10-28 03:14:45 +0200

    commit msg

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - sdh svd iushvdiuc hisufhdvihcsd
+ * @argc: sfjd ioj iowjfsio joiwjsfd
+ * @argv: oisdjf osdf hwdiusfhiu sfd
+ * Return: wkhd hfc jwhdf dhu
+*/
+int main(int argc, char const *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (-1);
+	}
+	return (0);
+}
