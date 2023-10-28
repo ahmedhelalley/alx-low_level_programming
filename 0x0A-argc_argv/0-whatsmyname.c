b1f52ec415e8d000044fe610a6b53befabc74da2
ahmedhelalley <a.helalley@gmail.com>
2023-10-28 02:52:19 +0200

    commit msg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#inlude < stdio.h >
+/**
+ * main - idjsf oihwd foid dsf ihdf
+ * @argc: lkwdjf jdf jf jdsifjcods
+ * @argv: lwjdf ojdfs opjwfdpo pjsd
+ * Return: wijd wfjds jds jc
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
