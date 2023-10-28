af07eb9d50a0f2e6b38cf2312f2e821b9f767212
ahmedhelalley <a.helalley@gmail.com>
2023-10-28 03:09:22 +0200

    commit msg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - oidsj foifd oiosdjfo cdsf
+ * @argc: wdjf jfdjs oeijfd cwds
+ * @argv: efsjd iorjsfd ijrfsd eoifjs
+ * Return: owjfe ojfwd ojwdf f
+*/
+int main(int argc, char const **argv)
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
