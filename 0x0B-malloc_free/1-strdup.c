8025c56a6a42560b53b7b31b3e5fe9df80b990bd
ahmedhelalley <a.helalley@gmail.com>
2023-11-01 13:56:32 +0200

    commit msg

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,10 +17,8 @@ char *_strdup(char *str)
 	m = malloc(size * sizeof(*str) + 1);
 	if (m == 0)
 		return (NULL);
-	else
-	{
-		for (; i < size; i++)
-			m[i] = str[i];
-	}
+	for (; i < size; i++)
+		m[i] = str[i];
+
 	return (m);
 }
