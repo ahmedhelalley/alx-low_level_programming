05613fce1763f4ece44a66308a906158e68576b2
ahmedhelalley <a.helalley@gmail.com>
2023-11-01 18:24:34 +0200

    commit msg

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -25,16 +25,16 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (; i < ac; i++; nc++)
+	for (; i < ac; i++, nc++)
 		nc += _strlen(av[i]);
 	s = malloc(sizeof(char) * nc + 1);
 	if (s == 0)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = o; av[i][j] != '\0'; j++, cmpt++)
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
 			s[cmpt] = av[i][j];
-		s[cmpt] = '\n'
+		s[cmpt] = '\n';
 		cmpt++;
 	}
 	return (s);
