eb9343a64bcfe577d83d78fec22953e5be0aae46
ahmedhelalley <a.helalley@gmail.com>
2023-11-01 19:19:11 +0200

    commit msg

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,71 +1,52 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
- * wrdcnt - osdh fhsdihf ihsuizdhxfv
- * @s: sdh fsdiofisdcoxi sdijf
- * Return: n
-*/
-int wrdcnt(char *s)
-{
-	int i, n = 0;
-
-	for (i = 0; s[i]; i++)
-	{
-		if (s[i] == ' ')
-		{
-			if (s[i + 1] != ' ' && s[i + 1] != '\0')
-				n++;
-		}
-		else if (i == 0)
-			n++;
-	}
-	n++;
-	return (n);
-}
-/**
- * **strtow - kdsjf kjsdcj oisjdiofjx sd
- * @str: osdjf oijsdifji shfvihisdhifv uhdsf
- * Return: w
+ * **strtow - ildshf hsdzfi fdgkndofi
+ * @str: idskj ioisdhicxv
+ * Return: words
 */
 char **strtow(char *str)
 {
-	int i, j, k, l, n = 0, wc = 0;
-	char **w;
-
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	n = wrdcnt(str);
-	if (n == 1)
-		return (NULL);
-	w = (char **)malloc(n * sizeof(char *));
-	if (w == NULL)
-		return (NULL);
-	w[n - 1] = NULL;
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-		{
-			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
-				;
-			j++;
-			w[wc] = (char *)malloc(j * sizeof(char));
-			j--;
-			if (w[wc] == NULL)
-			{
-				for (k = 0; k < wc; k++)
-					free(w[k]);
-				free(w[n - 1]);
-				free(w);
-				return (NULL);
-			}
-			for (l = 0; l < j; l++)
-				w[wc][l] = str[i + 1];
-			w[wc][l] = '\0';
-			wc++;
-			i += j;
-		}
-		else
-			i++;
-	}
-	return (w);
+if (str == NULL || str[0] == '\0')
+{
+return (NULL);
+}
+int wordCount = 1;
+for (int i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ')
+{
+wordCount++;
+while (str[i] == ' ')
+{
+i++;
+}
+}
+}
+char **words = (char **)malloc((wordCount + 1) * sizeof(char *));
+if (words == NULL)
+{
+return (NULL);
+}
+char *token = strtok(str, " ");
+int wordIndex = 0;
+while (token != NULL)
+{
+words[wordIndex] = (char *)malloc(strlen(token) + 1);
+if (words[wordIndex] == NULL)
+{
+for (int i = 0; i < wordIndex; i++)
+{
+free(words[i]);
+}
+free(words);
+return (NULL);
+}
+strcpy(words[wordIndex], token);
+wordIndex++;
+token = strtok(NULL, " ");
+}
+words[wordCount] = NULL;
+return (words);
 }
