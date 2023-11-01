7ce5508aba8f875a8f130cd1850780dbe18517b2
ahmedhelalley <a.helalley@gmail.com>
2023-11-01 16:01:06 +0200

    commit msg

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * **alloc_grid - ksdn kzcn ncjnvjsndv
+ * @width: lsdk jklj jikdsjoif
+ * @height: sdj ijisj km
+ * Return: tab
+*/
+int **alloc_grid(int width, int height)
+{
+	int **tab, i, j;
+
+	tab = malloc(sizeof(*tab) * height);
+	if (width == 0 || height == 0 || tab == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			tab[i] = malloc(sizeof(**tab) * width);
+			if (tab[i] == 0)
+			{
+				while (i--)
+					free(tab[i]);
+				free(tab);
+				return (NULL);
+			}
+			for (j = 0; j < width; j++)
+				tab[i][j] = 0;
+		}
+	}
+	return (tab);
+}
