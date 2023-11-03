92112a70d24823c046ac32a5608fa88ea1a4d31c
ahmedhelalley <a.helalley@gmail.com>
2023-11-03 04:28:12 +0200

    commit msg

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,6 +20,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
 		;
 	str = malloc(s1_length + n + 1);
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
 	for (j = 0; j < n; j++)
