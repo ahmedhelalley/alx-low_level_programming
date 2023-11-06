66af0037c6d8dc27097ba9e43665af4dc31409ff
ahmedhelalley <a.helalley@gmail.com>
2023-11-06 23:52:45 +0200

    commit msg

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - kj hsdkjhxijsbdvbbsdhcv
+ * @d: ksdhf jhsdjvhjdfv
+ * @name: lsdkch kjsdbjcvb
+ * @age: afe if a fog
+ * @owner: owner if the fog
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
