7b2b11f36b50bb468db49750efa0ce6f333316cf
ahmedhelalley <a.helalley@gmail.com>
2023-11-09 02:13:45 +0200

    commit msg

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_name - odsknaf iusdb suvubsivc
+ * @name: sldkf lksvk ndsnfvk ndfv
+ * @f: dlsknf nkjdvnkkfdnvknd
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
