f0861b2b63a71af5e3c20fa2fcab1c41f1dd42fd
ahmedhelalley <a.helalley@gmail.com>
2023-11-09 03:47:59 +0200

    commit msg

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add intereerds
+ * @a: skdf sv
+ * @b: dsnf jnfv
+ * Return: rsult
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - odsfn odoij ifd
+ * @a: dfn nsdnfi fvc
+ * @b: sencinfd nimebert
+ * Return: srestilt
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multedfijooksopg anogioninmerds
+ * @a: sldkf sdv sfkvc
+ * @b: sedcofc nikmet
+ * Return: restlit
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - diksdnvkllvc  wrgdwskm inrstdgv
+ * @a: fcofsdg jimrts
+ * @b: sedcomf im ertgvsd
+ * Return: srdfsdilgjltr
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - kwesjd ohsdai isdficasd
+ * @a: oiweha oho fv
+ * @b: wfda oeifdv oiefv
+ * Return: skjfjh uh
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
