3817ddb8e1a857a1f00009fcbf682f5d7d139808
ahmedhelalley <a.helalley@gmail.com>
2023-11-10 02:04:44 +0200

    commit msg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum all arguments
+ * @n: number of passed arguments
+ * @...: arguments to be passed (eppilipsies)
+ * Return: sum
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	int s = 0, i = n;
+	va_list ap;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+	while (i--)
+		s += va_arg(ap, int);
+	va_end(ap);
+	return (s);
+}
