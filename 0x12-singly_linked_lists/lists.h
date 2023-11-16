09a7ad74c68fc6294e5dcbb93fc2ed34f02e6fb7
ahmedhelalley <a.helalley@gmail.com>
2023-11-16 13:41:26 +0200

    commit msg

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,14 +8,13 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- */
+*/
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 int _putchar(char c);
 int _strlen(char *s);
@@ -25,4 +24,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* MAIN_H */
+#endif
