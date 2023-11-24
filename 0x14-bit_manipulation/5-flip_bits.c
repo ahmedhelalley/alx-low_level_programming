aeeb95ff01b2e15e478bece6a1b1ceeae55ffdf2
ahmedhelalley <a.helalley@gmail.com>
2023-11-24 20:52:09 +0200

    commit msg

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - flipping to reach a number
+ * @n: first number
+ * @m: second number
+ * Return: count
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xorval = n ^ m;
+	unsigned int count = 0;
+
+	while (xorval)
+	{
+		if (xorval & 1ul)
+			count++;
+		xorval = xorval >> 1;
+	}
+	return (count);
+}
