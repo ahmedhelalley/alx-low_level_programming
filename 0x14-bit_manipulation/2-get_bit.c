8c74dff185afe2c96ea89dd94730bd3ffc8a9ab3
ahmedhelalley <a.helalley@gmail.com>
2023-11-24 19:46:26 +0200

    commit msg

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_bit - getting a bit at an index
+ * @n: number
+ * @index: index
+ * Return: bit at index
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	return (n >> 1 & index);
+}
