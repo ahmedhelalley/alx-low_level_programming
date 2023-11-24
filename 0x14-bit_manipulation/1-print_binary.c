8b274b12e1833bd5c7805d9f62f64472910a1ee0
ahmedhelalley <a.helalley@gmail.com>
2023-11-24 19:35:24 +0200

    commit msg

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_binary - printing a number in binary form
+ * @n: number to be converted
+*/
+void print_binary(unsigned long int n)
+{
+	int bit = sizeof(n) * 8, printed = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			printed++;
+		}
+		else if (printed)
+			_putchar('0');
+	}
+	if (!printed)
+		_putchar('0');
+}
