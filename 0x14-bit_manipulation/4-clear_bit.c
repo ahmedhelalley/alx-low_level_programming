c3861e14748ea27978f54cf817efaafa9df72181
ahmedhelalley <a.helalley@gmail.com>
2023-11-24 20:42:32 +0200

    commit msg

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * clear_bit - clearing a bit
+ * @n: number
+ * @index: index
+ * Return: 1
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	if (*n & 1L << index)
+		*n ^= << index;
+	return (1);
+}
