361fd6773300a05b9f88762fd16e31651d8dbe87
ahmedhelalley <a.helalley@gmail.com>
2023-11-24 20:04:24 +0200

    commit msg

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_bit - setting a bit to 1 at an index
+ * @n: number passed
+ * @index: index
+ * Return: 1 or -1
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	return (*n |= 1L << index);
+}
