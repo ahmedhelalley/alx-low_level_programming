4976b1f5ed205b746f79c25609bc99fdf594a9c4
ahmedhelalley <a.helalley@gmail.com>
2023-11-27 23:25:32 +0200

    commit msg

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strlen - find length of a string
+ * @s: string passed
+ * Return: length i
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * create_file - creating a file
+ * @filename: file name
+ * @text_content: text that will be written
+ * Return: -1 on failure and 1 on success
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
