5083d9b72ebfb3f72cbe80cc2332f028ac0bb580
ahmedhelalley <a.helalley@gmail.com>
2024-01-12 03:07:27 +0200

    msg

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds a new head node to dll
+ * @head: pointer to the address of pointer to current head node
+ * @n: int field of the new node
+ * Return: address of the new node or NULL
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (!head || !new)
+		return (new ? free(new), NULL : NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	if (!*head)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
+	return (new);
+}
